4271bfe5d97b2ebd626a2297c25e98c31dfd6ec5
Liberbell <liberbell@gmail.com>
2020-05-23 11:19:29 +0900

    calls1

---
+++ calls.py
@@ -0,0 +1,9 @@
+def factorial(number):
+    if number < 0:
+        print("Sorry, factorial does not exist for negative numbers")
+        return
+
+    if number == 0:
+        return 1
+
+    return number * factorial(number - 1)
