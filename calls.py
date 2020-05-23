b6566e6669b95702aabfbbcc7793bc4857801047
Liberbell <liberbell@gmail.com>
2020-05-23 11:29:34 +0900

    calls4

--- calls.py
+++ calls.py
@@ -8,4 +8,14 @@ def factorial(number):
 
     return number * factorial(number - 1)
 
-print(factorial(-1))
+# print(factorial(10))
+
+def fibonacci(number):
+    if number < 2:
+        return
+
+    l = len(fib_series)
+
+    new_number = fib_series[l - 1] + fib_series[l - 2]
+    fib_series.append(new_number)
+    fibonacci(number - 1, fib_series)
