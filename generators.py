40b77c18866ad04d42dd16059b4d886245a22024
Liberbell <liberbell@gmail.com>
2020-05-24 15:22:32 +0900

    generator8

--- generators.py
+++ generators.py
@@ -8,5 +8,9 @@ g = geterate_even_numbers(7)
 # print(next(g))
 # print(next(g))
 # print(next(g))
-l = list(g)
-print(l)
+# l = list(g)
+# print(l)
+
+def geterate_squares(limit):
+    for i in range(0, limit, 2):
+        yield i ** 2
