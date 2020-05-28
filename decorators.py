8909feb321ec454a306146054306734a62e4b2ab
Liberbell <liberbell@gmail.com>
2020-05-28 09:45:47 +0900

    decorators3

--- decorators.py
+++ decorators.py
@@ -1,4 +1,10 @@
+import random
+
 def print_message():
     print("Yoohoo! Decorators are so cool.")
 
 print_message()
+
+def highlight():
+    annotaitions = ["-", "+", "*", "^", ":"]
+    annotate = random.choice(annotaitions)
