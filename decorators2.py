04f7c534d926e5d377d53fc7025ba004f6aa81c2
Liberbell <liberbell@gmail.com>
2020-05-31 14:25:42 +0900

    decorators2-4

--- decorators2.py
+++ decorators2.py
@@ -7,3 +7,13 @@ def astarisk_highlight(func):
         print("*" * 50)
 
     return highlight
+
+def plus_highlight(func):
+    def highlight():
+        print("+" * 50)
+
+        func()
+
+        print("+" * 50)
+    
+    return highlight
